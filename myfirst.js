e2a75070a397fc469d0fd5c4d24d70f5a5396cd1
Wasim Khan <wasimkhan1902@gmail.com>
2018-08-13 19:06:51 +0530

    Delete myfirst.js

--- myfirst.js
+++
@@ -1,8 +0,0 @@
-var http  = require('http'); 
-var dt = require('./myfirstModule.js');
-
-http.createServer(function(req,res){
-    res.writeHead(200, {'Content-type': 'text/plain'});
-    res.write('Displaying The today Date ' + dt.myDateModule()+'\n');
-    res.end('Hello World!');
-}).listen(8081);
